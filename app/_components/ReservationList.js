71c8789a3c5be77b0e6f936bec7772808188d27a
Pablo Fiterman <pfiterman@gmail.com>
2025-03-28 18:40:43 -0400

    Initial commit: Converting project into a product

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,33 @@
+"use client";
+
+import { useOptimistic } from "react";
+import { deleteBooking } from "../_lib/actions";
+import ReservationCard from "./ReservationCard";
+
+function ReservationList({ bookings }) {
+  const [optmisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteBooking(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optmisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          onDelete={handleDelete}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
---
+++ app/layout.js
@@ -0,0 +1,36 @@
+import { Josefin_Sans } from "next/font/google";
+import Header from "@/app/_components/Header";
+import { ReservationProvider } from "@/app/_components/ReservationContext";
+
+import "@/app/_styles/globals.css";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolimites, surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <di className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
+        </di>
+      </body>
+    </html>
+  );
+}
